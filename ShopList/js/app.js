f019851e6aee1aa0d0ed9a9e18dc722fc7debc52
HilbertSpaces <Jktecon@gmail.com>
2016-08-03 16:49:54 -0400

    complete ShopList

--- ShopList/js/app.js
+++ ShopList/js/app.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 	var k=0;
+	var r=0;
 	$(".submit").click(function(){
 		k+=1;
 		var text=$('.item').val();
@@ -14,16 +15,31 @@ $(document).ready(function() {
 			k+=1;
 			var text=$('.item').val();
 			$(".left").append("<li>"+text+"</li>");
-			if (k>20) {
+			if (k>20 && k>r) {
 				$(".boxl").css({"height":k*23});
 				$(".boxr").css({"height":k*23});
 			}
+			else if (k<r){
+				$(".boxl").css({"height":r*23});
+				$(".boxr").css({"height":r*23});
+			}
 		$(".item").val("");	
+		$("html").css({"background-size":"cover"});
 		}
 	});
 	$("ol").on("click","li",function(){
 		$(".right").append("<li>"+$(this).text()+"</li>");
 		this.remove();
+			if (k>20 && k>r) {
+				$(".boxl").css({"height":k*23});
+				$(".boxr").css({"height":k*23});
+			}
+			else if (k<r){
+				$(".boxl").css({"height":r*23});
+				$(".boxr").css({"height":r*23});
+			}
+		$("html").css({"background-size":"cover"});
+		r+=1;
 		k-=1;
 	});
 setTimeout(function (){	alert("Click on individual items within \"Things I need to Buy\" to add that item to your cart...Happy Shopping!");
