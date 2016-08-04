bd1aab8eca7f5e75d956e2b7b2f535fcdafef36f
HilbertSpaces <Jktecon@gmail.com>
2016-08-04 17:51:12 -0400

    Fizz Refactor

--- FizzBuzz/js/app.js
+++ FizzBuzz/js/app.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
+	alert("Click the page to enter a number");
 	$("body").click(function(){
+			x=Math.round(parseInt(prompt()));
 			var i=0;
 			var fizz="fizz";
 			var buzz="buzz";
-			while (i<=100){
+			while (i<=x){
 				if (i%3==0 && i%5==0){
 					$("body").append(fizz + " "+ buzz+"<br>");
 				}
